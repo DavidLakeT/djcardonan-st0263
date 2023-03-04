6722805c87d1bc005a76bcc86f6c20532fc9a29e
David Cardona N <djcardonan@eafit.edu.co>
2023-03-04 16:56:37 -0500

    Restructured files and added MOM basic implementation

---
+++ RPC/client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost:9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Error al conectarse: %v", err)
+	}
+
+	defer conn.Close()
+	//c := pb.NewFilesInformationClient(conn)
+
+	/*
+		userInfo := &pb.ListFilesRequest{}
+		response, err := c.ListFiles(context.Background(), userInfo)
+		if err != nil {
+			log.Fatalf("Error al obtener información de usuario: %v", err)
+		}
+		log.Printf("Respuesta (cantidad): %v", response.FileAmount)
+		log.Printf("Respuesta (lista): %v", response.FileList)
+	*/
+
+}
